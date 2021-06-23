3b64d66a3052c879243ab7807f6f0cf135892311
lvzhangyixing <1515622520@qq.com>
2021-06-23 21:19:21 +0800

    prime

--- ch4/09.cpp
+++ ch4/09.cpp
@@ -3,6 +3,7 @@
 //
 
 // 获取100个素数
+//主要在于理解通过指针访问数组  *(pointer + number)
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -19,8 +20,23 @@ int main() {
     {
         trial += 2; //获取下一个需要判断的数
 
+        // 循环3次，尝试当前数字能否被2，3，5整除
+        for (int i = 0; i < count; i++) {
+            found = (trial % *(primes + i) == 0); //如果能被2，3，5整除，将True赋值给found
+                if (found)
+                    break; //不是素数即跳出当前判断素数循环
+        }
+                if (!found)
+                    *(primes + count++) = trial;
+    }while (count < MAX); //循环MAX次，以获得100个素数
 
-    };
+    // 5个一行，显示所有的100个素数
+    for (int i = 0; i < MAX; i++){
+        if (i % 5 == 0)
+            cout << endl; // 5个一换行
+        cout << setw(10) << *(primes + i)    ;
+    }
+    cout << endl;
 
     return 0;
 
